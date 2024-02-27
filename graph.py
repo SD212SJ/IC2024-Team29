ee5c2bdf06facbb3563b1e4ecb2bd52bd5549bca
m263072 <142856247+m263072@users.noreply.github.com>
2024-02-27 17:59:26 -0500

    Add files via upload

--- graph.py
+++ graph.py
@@ -2,8 +2,14 @@ import pandas as pd
 
 df = pd.read_csv('really_really_filtered.csv')
 
-count1 = 0 
-for each in df['food_category_id']:
-    if each == 1: 
-        print(each[df['SR Mean per 100g']])
+onefood = df[df['food_category_id'] == 1]
+#summing the SR Mean by foodcategory id and the nutrient 
+SReachcat = df.groupby(['food_category_id','SR Food description'])['SR Mean per 100g'].sum()
+FFeachcat = df.groupby(['food_category_id','SR Food description'])['FF Mean per 100g'].sum()
+
+print(SReachcat)
+#this divides all the sums of nutrients by the total number of rows that include foodcategory = 1 so this is wrong
+#need to do something like if the category is 1, divide the 
+
+# print(FFeachcat / (len(onefood)))
         
