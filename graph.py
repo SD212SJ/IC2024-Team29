170dd008d653df09e1eb3210b7de86684c48a6a0
m263072 <142856247+m263072@users.noreply.github.com>
2024-02-27 21:34:46 -0500

    Add files via upload

--- graph.py
+++ graph.py
@@ -1,15 +1,14 @@
 import pandas as pd
-
+import plotly.express as px
 df = pd.read_csv('really_really_filtered.csv')
 
-onefood = df[df['food_category_id'] == 1]
-#summing the SR Mean by foodcategory id and the nutrient 
-SReachcat = df.groupby(['food_category_id','SR Food description'])['SR Mean per 100g'].sum()
-FFeachcat = df.groupby(['food_category_id','SR Food description'])['FF Mean per 100g'].sum()
+#summing the SR Mean by foodcategory id and the nutrient and putting them into new columns
+SReachcat = df.groupby(['food_category_id','SR Food description'])['SR Mean per 100g'].sum().reset_index()
+SReachcat.columns = ['food_category_id', 'SR Food description','SReachcat']
+FFeachcat = df.groupby(['food_category_id','SR Food description'])['FF Mean per 100g'].sum().reset_index()
+FFeachcat.columns = ['food_category_id', 'SR Food description','SReachcat']
 
-print(SReachcat)
-#this divides all the sums of nutrients by the total number of rows that include foodcategory = 1 so this is wrong
-#need to do something like if the category is 1, divide the 
+df = pd.merge(df,SReachcat, on=['food_category_id','SR Food description'], how='left')
+df = pd.merge(df,FFeachcat, on=['food_category_id','SR Food description'], how='left')
 
-# print(FFeachcat / (len(onefood)))
-        
+df.to_csv('new_file.csv', index=False)
