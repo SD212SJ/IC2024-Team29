39597ebcb6093d9ee47a0599dbdd70cd81e2c7dc
SD212SJ <160253369+SD212SJ@users.noreply.github.com>
2024-03-02 00:19:48 -0500

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -1,19 +1,13 @@
 
 #1. filtering out the data 
 
-#cut -d',' -f3,5 'FF_SR_ data.csv' > filtered.csv
-#sed -e 's/,,,,,,,,,,,,,,,,,,//g' filtered.csv > new_filtered.csv
-#grep -e ',1,".*2019' "FF_SR_ data.csv" > data.csv
-#grep -e '\b.*/20[0-9]{2}' new_filtered.csv 
+#cut -d',' -f3,5 'FF_SR_ data.csv' > 2filter.csv
+#sed -e 's/,,,,,,,,,,,,,,,,,,//g' filtered.csv > 2filter.csv
+#grep -e ',1,".*2019' "FF_SR_ data.csv" > 2filter.csv
+#grep -e '\b.*/20[0-9]{2}' 2filter.csv
 
-#grep -e '16158,16158,16,"Hummus, commercial' 'FF_SR_ data.csv' > sample.csv
 
-grep -E -i 'Vitamin A' 'filtered.csv' > new_filtered.csv
-grep -E -i 'Vitamin D' 'filtered.csv' >> new_filtered.csv
-grep -E -i 'Vitamin B' 'filtered.csv' >> new_filtered.csv
-grep -E -i 'calcium' 'filtered.csv' >> new_filtered.csv
-grep -E -i 'iron' 'filtered.csv' >> new_filtered.csv
-grep -E -i 'zinc' 'filtered.csv' >> new_filtered.csv
+
 
 
 
